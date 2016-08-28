58a239ca0dd0c799b40039b4d026ab686044b901
kartik <kartik.gupta@iitrpr.ac.in>
2016-08-29 04:00:14 +0530

    Added MoviePosterGridFragment and made a call to API

--- app/src/main/java/com/kartikgupta/popularmovies/MainActivity.java
+++ app/src/main/java/com/kartikgupta/popularmovies/MainActivity.java
@@ -1,5 +1,8 @@
 package com.kartikgupta.popularmovies;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridFragment gridFragment = new GridFragment();
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeholder,gridFragment,"MOVIE_GRID");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+
     }
 }
