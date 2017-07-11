d66ec1b3e70ecf3bdb91ac5a3b086b1e66b108b6
kartik <kartik.gupta@iitrpr.ac.in>
2017-07-11 21:16:07 +0530

    update getMovie function to successfully fetch popular movie data

--- app/src/main/java/com/kartikgupta/popularmovies/MainActivity.java
+++ app/src/main/java/com/kartikgupta/popularmovies/MainActivity.java
@@ -6,13 +6,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,29 +37,38 @@ public class MainActivity extends AppCompatActivity {
     private void getMovieData() {
             OkHttpClient client = new OkHttpClient();
 
-            Request request = new Request.Builder()
-                    .url(BASE_URL+mSortOrder)
-                    .addHeader("api_key",API_KEY)
-                    .addHeader("language","en-US")
-                    .addHeader("page","1")
-                    .build();
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("api_key", API_KEY)
+                .addFormDataPart("language", "en-US")
+                .addFormDataPart("page", "1")
+                .build();
 
 
-            client.newCall(request).enqueue(new Callback() {
-                @Override public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
+        Request request = new Request.Builder()
+                .url(BASE_URL + mSortOrder)
+                .post(requestBody)
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                Headers responseHeaders = response.headers();
+                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
                 }
 
-                @Override public void onResponse(Call call, Response response) throws IOException {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                System.out.println(response.body().string());
+            }
+        });
 
-                    Headers responseHeaders = response.headers();
-                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                    }
-
-                    System.out.println(response.body().string());
-                }
-            });
     }
 }
